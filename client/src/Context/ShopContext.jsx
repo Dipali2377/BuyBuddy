845b7116e190183fe7b4b305d880c954518c5d5d
Dipali2377 <dipalim680@gmail.com>
2025-05-13 20:03:11 +0530

    Products data added

---
+++ client/src/Context/ShopContext.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createContext } from "vm";
+
+const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+  const contextValue = {};
+};
+
+export default ShopContext;
--- client/src/assets/all_product.js
+++ client/src/assets/all_product.js
@@ -1,9 +1,46 @@
+import p1_img from "./All_Products_Images/p1_img.png";
+import p2_img from "./All_Products_Images/p2_img.png";
+import p3_img from "./All_Products_Images/p3_img.png";
+import p4_img from "./All_Products_Images/p4_img.png";
+import p5_img from "./All_Products_Images/p5_img.png";
+import p6_img from "./All_Products_Images/p6_img.png";
+import p7_img from "./All_Products_Images/p7_img.png";
+import p8_img from "./All_Products_Images/p8_img.png";
+import p9_img from "./All_Products_Images/p9_img.png";
+import p10_img from "./All_Products_Images/p10_img.png";
+import p11_img from "./All_Products_Images/p11_img.png";
+import p12_img from "./All_Products_Images/p12_img.png";
+import p13_img from "./All_Products_Images/p13_img.png";
+import p14_img from "./All_Products_Images/p14_img.png";
+import p15_img from "./All_Products_Images/p15_img.png";
+import p16_img from "./All_Products_Images/p16_img.png";
+import p17_img from "./All_Products_Images/p17_img.png";
+import p18_img from "./All_Products_Images/p18_img.png";
+import p19_img from "./All_Products_Images/p19_img.png";
+import p20_img from "./All_Products_Images/p20_img.png";
+import p21_img from "./All_Products_Images/p21_img.png";
+import p22_img from "./All_Products_Images/p22_img.png";
+import p23_img from "./All_Products_Images/p23_img.png";
+import p24_img from "./All_Products_Images/p24_img.png";
+import p25_img from "./All_Products_Images/p25_img.png";
+import p26_img from "./All_Products_Images/p26_img.png";
+import p27_img from "./All_Products_Images/p27_img.png";
+import p28_img from "./All_Products_Images/p28_img.png";
+import p29_img from "./All_Products_Images/p29_img.png";
+import p30_img from "./All_Products_Images/p30_img.png";
+import p31_img from "./All_Products_Images/p31_img.png";
+import p32_img from "./All_Products_Images/p32_img.png";
+import p33_img from "./All_Products_Images/p33_img.png";
+import p34_img from "./All_Products_Images/p34_img.png";
+import p35_img from "./All_Products_Images/p35_img.png";
+import p36_img from "./All_Products_Images/p36_img.png";
+
 let all_product = [
   {
     id: 1,
     name: "Boys Blue Denim Jacket",
     category: "kid",
-    image: "p1_img",
+    image: p1_img,
     new_price: 70.0,
     old_price: 100.0,
   },
@@ -11,7 +48,7 @@ let all_product = [
     id: 2,
     name: "Girls Pink Party Dress",
     category: "kid",
-    image: "p2_img",
+    image: p2_img,
     new_price: 90.0,
     old_price: 130.0,
   },
@@ -19,7 +56,7 @@ let all_product = [
     id: 3,
     name: "Men's Black Leather Jacket",
     category: "men",
-    image: "p3_img",
+    image: p3_img,
     new_price: 150.0,
     old_price: 220.0,
   },
@@ -27,7 +64,7 @@ let all_product = [
     id: 4,
     name: "Women's Floral Summer Dress",
     category: "women",
-    image: "p4_img",
+    image: p4_img,
     new_price: 95.0,
     old_price: 140.0,
   },
@@ -35,7 +72,7 @@ let all_product = [
     id: 5,
     name: "Men's Slim Fit Shirt",
     category: "men",
-    image: "p5_img",
+    image: p5_img,
     new_price: 55.0,
     old_price: 80.0,
   },
@@ -43,7 +80,7 @@ let all_product = [
     id: 6,
     name: "Women's High Waist Jeans",
     category: "women",
-    image: "p6_img",
+    image: p6_img,
     new_price: 75.0,
     old_price: 105.0,
   },
@@ -51,7 +88,7 @@ let all_product = [
     id: 7,
     name: "Boys Cotton T-Shirt",
     category: "kid",
-    image: "p7_img",
+    image: p7_img,
     new_price: 30.0,
     old_price: 50.0,
   },
@@ -59,7 +96,7 @@ let all_product = [
     id: 8,
     name: "Girls Sequin Top",
     category: "kid",
-    image: "p8_img",
+    image: p8_img,
     new_price: 40.0,
     old_price: 65.0,
   },
@@ -67,7 +104,7 @@ let all_product = [
     id: 9,
     name: "Men's Checked Casual Shirt",
     category: "men",
-    image: "p9_img",
+    image: p9_img,
     new_price: 60.0,
     old_price: 90.0,
   },
@@ -75,7 +112,7 @@ let all_product = [
     id: 10,
     name: "Women's Elegant Gown",
     category: "women",
-    image: "p10_img",
+    image: p10_img,
     new_price: 120.0,
     old_price: 180.0,
   },
@@ -83,7 +120,7 @@ let all_product = [
     id: 11,
     name: "Men's Running Sneakers",
     category: "men",
-    image: "p11_img",
+    image: p11_img,
     new_price: 110.0,
     old_price: 150.0,
   },
@@ -91,7 +128,7 @@ let all_product = [
     id: 12,
     name: "Women's Stylish Handbag",
     category: "women",
-    image: "p12_img",
+    image: p12_img,
     new_price: 85.0,
     old_price: 130.0,
   },
@@ -99,7 +136,7 @@ let all_product = [
     id: 13,
     name: "Boys Printed Hoodie",
     category: "kid",
-    image: "p13_img",
+    image: p13_img,
     new_price: 45.0,
     old_price: 70.0,
   },
@@ -107,7 +144,7 @@ let all_product = [
     id: 14,
     name: "Girls Ballet Flats",
     category: "kid",
-    image: "p14_img",
+    image: p14_img,
     new_price: 50.0,
     old_price: 75.0,
   },
@@ -115,7 +152,7 @@ let all_product = [
     id: 15,
     name: "Men's Formal Trousers",
     category: "men",
-    image: "p15_img",
+    image: p15_img,
     new_price: 80.0,
     old_price: 120.0,
   },
@@ -123,7 +160,7 @@ let all_product = [
     id: 16,
     name: "Women's Office Blazer",
     category: "women",
-    image: "p16_img",
+    image: p16_img,
     new_price: 110.0,
     old_price: 160.0,
   },
@@ -131,7 +168,7 @@ let all_product = [
     id: 17,
     name: "Boys Cotton Shorts",
     category: "kid",
-    image: "p17_img",
+    image: p17_img,
     new_price: 25.0,
     old_price: 45.0,
   },
@@ -139,7 +176,7 @@ let all_product = [
     id: 18,
     name: "Girls Embroidered Kurti",
     category: "kid",
-    image: "p18_img",
+    image: p18_img,
     new_price: 55.0,
     old_price: 90.0,
   },
@@ -147,7 +184,7 @@ let all_product = [
     id: 19,
     name: "Men's Woolen Sweater",
     category: "men",
-    image: "p19_img",
+    image: p19_img,
     new_price: 95.0,
     old_price: 140.0,
   },
@@ -155,7 +192,7 @@ let all_product = [
     id: 20,
     name: "Women's Knitted Cardigan",
     category: "women",
-    image: "p20_img",
+    image: p20_img,
     new_price: 85.0,
     old_price: 120.0,
   },
@@ -163,7 +200,7 @@ let all_product = [
     id: 21,
     name: "Boys Striped Shirt",
     category: "kid",
-    image: "p21_img",
+    image: p21_img,
     new_price: 35.0,
     old_price: 55.0,
   },
@@ -171,7 +208,7 @@ let all_product = [
     id: 22,
     name: "Girls Denim Skirt",
     category: "kid",
-    image: "p22_img",
+    image: p22_img,
     new_price: 40.0,
     old_price: 65.0,
   },
@@ -179,7 +216,7 @@ let all_product = [
     id: 23,
     name: "Men's Casual Loafers",
     category: "men",
-    image: "p23_img",
+    image: p23_img,
     new_price: 100.0,
     old_price: 145.0,
   },
@@ -187,7 +224,7 @@ let all_product = [
     id: 24,
     name: "Women's Sports Shoes",
     category: "women",
-    image: "p24_img",
+    image: p24_img,
     new_price: 95.0,
     old_price: 130.0,
   },
@@ -195,7 +232,7 @@ let all_product = [
     id: 25,
     name: "Boys Winter Coat",
     category: "kid",
-    image: "p25_img",
+    image: p25_img,
     new_price: 120.0,
     old_price: 170.0,
   },
@@ -203,7 +240,7 @@ let all_product = [
     id: 26,
     name: "Girls Winter Boots",
     category: "kid",
-    image: "p26_img",
+    image: p26_img,
     new_price: 75.0,
     old_price: 110.0,
   },
@@ -211,7 +248,7 @@ let all_product = [
     id: 27,
     name: "Men's Slim Fit Jeans",
     category: "men",
-    image: "p27_img",
+    image: p27_img,
     new_price: 85.0,
     old_price: 125.0,
   },
@@ -219,7 +256,7 @@ let all_product = [
     id: 28,
     name: "Women's Maxi Dress",
     category: "women",
-    image: "p28_img",
+    image: p28_img,
     new_price: 105.0,
     old_price: 155.0,
   },
@@ -227,7 +264,7 @@ let all_product = [
     id: 29,
     name: "Boys Orange Colourblocked Hoodie",
     category: "kid",
-    image: "p29_img",
+    image: p29_img,
     new_price: 85.0,
     old_price: 120.5,
   },
@@ -235,7 +272,7 @@ let all_product = [
     id: 30,
     name: "Girls Graphic Print T-shirt",
     category: "kid",
-    image: "p30_img",
+    image: p30_img,
     new_price: 35.0,
     old_price: 55.0,
   },
@@ -243,7 +280,7 @@ let all_product = [
     id: 31,
     name: "Men's Hooded Sweatshirt",
     category: "men",
-    image: "p31_img",
+    image: p31_img,
     new_price: 70.0,
     old_price: 100.0,
   },
@@ -251,7 +288,7 @@ let all_product = [
     id: 32,
     name: "Women's Faux Leather Jacket",
     category: "women",
-    image: "p32_img",
+    image: p32_img,
     new_price: 130.0,
     old_price: 180.0,
   },
@@ -259,7 +296,7 @@ let all_product = [
     id: 33,
     name: "Boys Full Sleeve Shirt",
     category: "kid",
-    image: "p33_img",
+    image: p33_img,
     new_price: 45.0,
     old_price: 65.0,
   },
@@ -267,7 +304,7 @@ let all_product = [
     id: 34,
     name: "Girls Casual Denim Jacket",
     category: "kid",
-    image: "p34_img",
+    image: p34_img,
     new_price: 80.0,
     old_price: 115.0,
   },
@@ -275,7 +312,7 @@ let all_product = [
     id: 35,
     name: "Men's Striped Polo T-shirt",
     category: "men",
-    image: "p35_img",
+    image: p35_img,
     new_price: 50.0,
     old_price: 75.0,
   },
@@ -283,8 +320,10 @@ let all_product = [
     id: 36,
     name: "Women's Embellished Top",
     category: "women",
-    image: "p36_img",
+    image: p36_img,
     new_price: 70.0,
     old_price: 100.0,
   },
 ];
+
+export default all_product;
